f8b4d36a78fd33589faf404e95229dbf856968ac
Neighfan2002 <79333520+Neighfan2002@users.noreply.github.com>
2021-02-19 19:17:23 +0000

    Add files via upload

---
+++ number-letter-thingy.py
@@ -0,0 +1,78 @@
+def TRANSLATE(NUMBER):
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten',
+          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen',
+          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen',
+          19 : 'nineteen', 20 : 'twenty',
+          30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty',
+          70 : 'seventy', 80 : 'eighty', 90 : 'ninety' }
+    THOUSAND = 1000
+    MILLION = 1000000
+    BILLION = 1000000000
+    TRILLION = 1000000000000
+    QUADRILLION = 1000000000000000
+
+    if (NUMBER < 20):
+        return d[NUMBER]
+
+    if (NUMBER < 100):
+        if NUMBER % 10 == 0:
+            return d[NUMBER]
+        else:
+            return d[NUMBER // 10 * 10] + '' + d[NUMBER % 10]
+
+    if (NUMBER < THOUSAND):
+        if NUMBER % 100 == 0:
+            return d[NUMBER // 100] + 'hundred'
+        else:
+            return d[NUMBER // 100] + 'hundredand' + TRANSLATE(NUMBER % 100)
+
+    if (NUMBER < MILLION):
+        if NUMBER % THOUSAND == 0:
+            return TRANSLATE(NUMBER // THOUSAND) + 'thousand'
+        else:
+            return TRANSLATE(NUMBER // THOUSAND) + 'thousand' + TRANSLATE(NUMBER % THOUSAND)
+
+    if (NUMBER < BILLION):
+        if (NUMBER % MILLION) == 0:
+            return TRANSLATE(NUMBER // MILLION) + 'million'
+        else:
+            return TRANSLATE(NUMBER // MILLION) + 'million' + TRANSLATE(NUMBER % MILLION)
+
+    if (NUMBER < TRILLION):
+        if (NUMBER % BILLION) == 0:
+            return TRANSLATE(NUMBER // BILLION) + 'billion'
+        else:
+            return TRANSLATE(NUMBER // BILLION) + 'billion' + TRANSLATE(NUMBER % BILLION)
+
+    if (NUMBER < QUADRILLION):
+        if (NUMBER % BILLION) == 0:
+            return TRANSLATE(NUMBER // TRILLION) + 'trillion'
+        else:
+            return TRANSLATE(NUMBER // TRILLION) + 'trillion' + TRANSLATE(NUMBER % TRILLION)
+
+    if (NUMBER % QUADRILLION == 0):
+        return TRANSLATE(NUMBER // QUADRILLION) + 'quadrillion'
+    else:
+        return TRANSLATE(NUMBER // QUADRILLION) + 'quadrillion' + TRANSLATE(NUMBER % QUADRILLION)
+
+def do():
+    start = TRANSLATE(int(input('NUMBER: ')))
+    new = start
+    on = 1
+    while on:
+        print(new)
+        old, new = new, TRANSLATE(len(new))
+        if old == new:
+            on = 0
+            
+while True:
+    do()
+
+
+
+
+
+
+
+    
